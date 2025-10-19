d1b956a616d0257173f454b5e55a609a85e11baf
AMANTRIPATHI12 <amantripathi2244@gmail.com>
2025-10-19 16:14:54 +0530

    controller update

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,33 +1,24 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
-import "dotenv/config";
+import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
-
   try {
     const token = req.cookies.jwt;
-
-    if (!token) {
+    if (!token)
       return res.status(401).json({ message: "Unauthorized - No token provided" });
-    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    if (!decoded) {
+    if (!decoded?.userId)
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
-    }
 
     const user = await User.findById(decoded.userId).select("-password");
-
-    if (!user) {
+    if (!user)
       return res.status(401).json({ message: "Unauthorized - User not found" });
-    }
 
     req.user = user;
-
     next();
   } catch (error) {
     console.log("Error in protectRoute middleware", error);
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(401).json({ message: "Unauthorized or token expired" });
   }
 };
